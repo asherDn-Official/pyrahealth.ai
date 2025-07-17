9978ae6495359c283bafc034a2c11ec39228a61c
praveen <170600761+asherDn-Official@users.noreply.github.com>
2025-07-17 16:59:55 +0530

    Update GrowthStepCard.jsx

--- src/screens/LandingPage/sections/CustomStrategiesSection/componets/ui/GrowthStepCard.jsx
+++ src/screens/LandingPage/sections/CustomStrategiesSection/componets/ui/GrowthStepCard.jsx
@@ -23,7 +23,7 @@ export const GrowthStepCard = ({ title, description, iconSrc, isLast = false }) 
 
   return (
     <div className="flex flex-col items-center p-2.5 w-full max-w-[273px] relative">
-       <Card className="w-full h-56 lg:h-64 rounded-[40px] shadow-[0px_4px_24.3px_#ffcebd] border-red-500 hover:shadow-[0px_8px_30px_#ffcebd] transition-shadow">
+      <Card className="w-full h-56 lg:h-64 rounded-[40px] shadow-[0px_4px_24.3px_#ffcebd] border-red-500 hover:shadow-[0px_8px_30px_#ffcebd] transition-shadow">
         <CardContent className="flex flex-col items-center gap-4 lg:gap-5 p-4 lg:p-5 h-full justify-center">
           <h3 className="font-bold text-[#121416] text-lg lg:text-xl text-center leading-6 lg:leading-[29px] max-w-[186px]">
             {title}
@@ -35,7 +35,7 @@ export const GrowthStepCard = ({ title, description, iconSrc, isLast = false }) 
       </Card>
 
       {isLast ? (
-       <div className="flex w-[70px] lg:w-[84px] h-[70px] lg:h-[84px] items-center justify-center -mt-4 bg-white rounded-[42px] shadow-[0px_4px_10.3px_#ffc1a6] border-none">
+        <div className="flex w-[70px] lg:w-[84px] h-[70px] lg:h-[84px] items-center justify-center -mt-4 bg-white rounded-[42px] shadow-[0px_4px_10.3px_#ffc1a6] border-none">
           <div className="relative w-[40px] lg:w-[50px] h-[40px] lg:h-[50px]">
             <img
               className="w-auto h-auto"
@@ -48,12 +48,13 @@ export const GrowthStepCard = ({ title, description, iconSrc, isLast = false }) 
         <div className="flex w-[70px] lg:w-[84px] h-[70px] lg:h-[84px] items-center justify-center -mt-4 bg-white rounded-[42px] shadow-[0px_4px_10.3px_#ffc1a6] border-none">
           <div className="relative w-[40px] lg:w-[50px] h-[40px] lg:h-[50px]">
             <img
-              className="w-auto h-auto"
+              className="w-full h-full object-contain"
               alt="Icon"
               src={icon}
             />
           </div>
         </div>
+
       )}
 
       {!isLast && (
