79e07da94ab670ca911ae499ecc753cb8999f5ad
praveen <170600761+asherDn-Official@users.noreply.github.com>
2025-07-17 12:34:16 +0530

    Update TestimonialCard.jsx

--- src/screens/LandingPage/sections/CustomStrategiesSection/componets/ui/TestimonialCard.jsx
+++ src/screens/LandingPage/sections/CustomStrategiesSection/componets/ui/TestimonialCard.jsx
@@ -7,7 +7,7 @@ export const TestimonialCard = ({ content, author, position, logo }) => {
   return (
     <Card className="w-full max-w-[569px] min-h-[400px] lg:h-[513px] rounded-[45px] border-4 border-[#ffecec] shadow-[0px_4px_40.1px_#ffe4df] bg-white hover:shadow-[0px_8px_50px_#ffe4df] transition-shadow flex-shrink-0">
       <CardContent className="flex flex-col items-center gap-6 lg:gap-[37px] p-6 lg:p-12 h-full justify-between">
-        <p className="text-[#121416] text-lg sm:text-xl lg:text-2xl text-center tracking-[-0.72px] leading-6 lg:leading-8 flex-grow flex items-center">
+        <p className="text-[#121416] text-lg sm:text-xl  text-justify tracking-[-0.72px] leading-6 lg:leading-8 flex-grow flex items-center">
           {content}
         </p>
 
